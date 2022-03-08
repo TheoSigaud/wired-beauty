3f9e638366b66081b69f5e2ae112273e4eb39dfa
Théo Sigaud <sigaudtheo@gmail.com>
2022-03-08 15:42:19 +0100

    Add choice graph

--- client/src/views/Dashboard/Dashboard.js
+++ client/src/views/Dashboard/Dashboard.js
@@ -3,10 +3,22 @@ import DashboardService from '@/services/DashboardService';
 export default {
   name: "Dashboard",
 
-  data () {
+  data() {
     return {
-      image: null
+      image: null,
+      showSelectChart: false,
+      showSelectAxes: false,
+      titles: [],
+      usersId: [],
+      typeChart: null,
+      axisX: null,
+      axisY: null,
+      selectUserId: null
     }
+  },
+
+  mounted() {
+    this.createChart();
   },
 
   methods: {
@@ -16,7 +28,21 @@ export default {
 
     async upload() {
       this.read().then(
-        data => console.log(data)
+        data => {
+          this.titles = data.split('\n')[0].split(',');
+          this.showSelectChart = true;
+
+          let arrayLine = data.split('\n');
+          arrayLine.shift();
+
+          arrayLine.forEach(element => {
+            element = element.split(',')[1];
+            if (this.usersId.indexOf(element) === -1) {
+              this.usersId.push(element)
+            }
+          });
+          console.log(this.usersId);
+        }
       );
     },
 
@@ -28,5 +54,31 @@ export default {
         reader.onerror = error => reject(error);
       });
     },
+
+    async selectChart(type) {
+      this.showSelectChart = false;
+      this.typeChart = type;
+      this.showSelectAxes = true;
+    },
+
+    async createChart() {
+      var ctx = document.getElementById('turnover').getContext('2d'); //Structure du graphique chiffre d'affaires
+      const chartTurnover = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: '',
+          datasets: [{
+            label: 'Chiffre d\'affaires',
+            data: '',
+            backgroundColor: 'rgba(111,207,151,0.3)',
+            borderColor: '#27AE60',
+            pointBackgroundColor: '#27AE60',
+            borderWidth: 1,
+            pointRadius: 5,
+            pointHoverRadius: 10
+          }]
+        },
+      })
+    }
   }
 }
--- client/src/views/Dashboard/Dashboard.vue
+++ client/src/views/Dashboard/Dashboard.vue
@@ -14,6 +14,82 @@
         </div>
       </div>
     </div>
+
+    <div v-if="showSelectChart">
+      <div class="container mt-5" @click="selectChart('vertical')">
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body">
+                <img src="@/assets/vertical-chart.png" width="80%">
+                <p style="color: black">Vertical bar</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container mt-5" @click="selectChart('line')">
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body">
+                <img src="@/assets/line-chart.png" width="80%">
+                <p style="color: black">Line chart</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container mt-5" @click="selectChart('scatter')">
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body">
+                <img src="@/assets/scatter-chart.png" width="80%">
+                <p style="color: black">Scatter chart</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div v-if="showSelectAxes">
+      <div class="container mt-5" @click="selectChart('scatter')">
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body">
+                <p style="color: black">User id</p>
+                <select v-model="selectUserId">
+                  <option value="all">Tout</option>
+                  <option v-for="userId in usersId" :value="userId" :key="userId">
+                    {{ userId }}
+                  </option>
+                </select>
+
+                <p style="color: black">Axe X</p>
+                <select v-model="axisX">
+                  <option v-for="title in titles" :value="title" :key="title">
+                    {{ title }}
+                  </option>
+                </select>
+
+                <p style="color: black">Axe Y</p>
+                <select v-model="axisY">
+                  <option v-for="title in titles" :value="title" :key="title">
+                    {{ title }}
+                  </option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <canvas id="turnover" width="170" height="60"></canvas>
   </div>
 </template>
 
