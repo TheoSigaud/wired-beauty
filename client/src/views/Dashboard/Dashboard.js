0fdc62aca49b61cd320904b69ff561cd1ebff2f6
Théo Sigaud <sigaudtheo@gmail.com>
2022-03-09 20:30:32 +0100

    Read xlsx

--- client/src/views/Dashboard/Dashboard.js
+++ client/src/views/Dashboard/Dashboard.js
@@ -1,4 +1,11 @@
 import DashboardService from '@/services/DashboardService';
+import * as XLSX from 'xlsx/xlsx.mjs';
+
+/* load 'fs' for readFile and writeFile support */
+import * as fs from 'fs';
+
+/* load the codepage support library for extended support with older formats  */
+import * as cpexcel from 'xlsx/dist/cpexcel.full.mjs';
 
 export default {
   name: "Dashboard",
@@ -7,33 +14,11 @@ export default {
     return {
       chart: null,
       file: null,
-      fileSplit: null,
-      fileShift: null,
       errorUpload: null,
-      showSelectChart: false,
-      showSelectAxes: false,
-      titles: [],
-      usersId: [],
-      typeChart: 'line',
-      axisX: null,
-      axisY: null,
-      selectUserId: null,
-      arrayX: [],
-      arrayY: [],
-      errors: {
-        x: null,
-        y: null,
-        user: null
-      },
-      arrayCharts: []
     }
   },
 
   methods: {
-    async addGraph() {
-      this.arrayCharts.push(this.chart.toBase64Image());
-      console.log(this.arrayCharts);
-    },
 
     async uploadFile(e) {
       this.file = e.target.files[0];
@@ -41,116 +26,9 @@ export default {
     },
 
     async upload() {
-      this.fileSplit = null;
-      this.showSelectChart = false;
-      this.showSelectAxes = false;
-      this.titles = [];
-      this.usersId = [];
-      this.typeChart = 'line';
-      this.axisX = null;
-      this.axisY = null;
-      this.selectUserId = null;
-      this.arrayX = [];
-      this.arrayY = [];
-      this.errors = {
-        x: null,
-        y: null,
-        user: null
-      }
-
-      if (this.chart !== null) {
-        this.chart.destroy();
-      }
-
-      this.read().then(
-        data => {
-          let nameSplit = this.file.name.split('.');
-
-          if (nameSplit[nameSplit.length - 1] === 'csv') {
-
-            this.titles = data.split('\n')[0].split(',');
-            this.showSelectChart = true;
-
-            let arrayLine = data.split('\n');
-            this.fileSplit = data.split('\n');
-            this.fileShift = data.split('\n');
-            this.fileShift.shift();
-            arrayLine.shift();
-
-
-            arrayLine.forEach(element => {
-              element = element.split(',');
-              element.forEach(x => {
-                if (x.startsWith('WB') && this.usersId.indexOf(x) === -1) {
-                  this.usersId.push(x)
-                }
-              })
-            });
-          } else {
-            this.errorUpload = 'Le fichier n\'est pas un CSV';
-          }
-        }
-      );
-    },
-
-    async read() {
-      return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.readAsText(this.file);
-        reader.onload = () => resolve(reader.result);
-        reader.onerror = error => reject(error);
-      });
-    },
-
-    async selectChart(type) {
-      this.showSelectChart = false;
-      this.typeChart = type;
-      this.showSelectAxes = true;
-    },
-
-    async generateGraph() {
-
-      this.errors.user = null;
-      this.errors.x = null;
-      this.errors.y = null;
-      this.arrayX = [];
-      this.arrayY = [];
-
-      if (this.axisX === null) {
-        this.errors.x = 'Le champ est obligatoire'
-      }
-
-      if (this.axisY === null) {
-        this.errors.y = 'Le champ est obligatoire'
-      }
-
-      if (this.selectUserId === null) {
-        this.errors.user = 'Le champ est obligatoire'
-      }
-
-
-      if (this.errors.user === null && this.errors.x === null && this.errors.y === null) {
-
-        let tmpTitles = this.fileSplit[0].split(',');
-
-        let posX = tmpTitles.indexOf(this.axisX);
-        let posY = tmpTitles.indexOf(this.axisY);
-
-        this.fileShift.forEach(element => {
-          let elementSplit = element.split(',');
-          if (this.selectUserId !== 'all') {
-            if (elementSplit.indexOf(this.selectUserId) !== -1) {
-              this.arrayX.push(elementSplit[posX]);
-              this.arrayY.push(elementSplit[posY]);
-            }
-          } else {
-            this.arrayX.push(elementSplit[posX]);
-            this.arrayY.push(elementSplit[posY]);
-          }
-        });
-
-        await this.createChart();
-      }
+      const data = await this.file.arrayBuffer();
+      const workbook = XLSX.read(data);
+      console.log(workbook);
     },
 
     async createChart() {
--- client/src/views/Dashboard/Dashboard.vue
+++ client/src/views/Dashboard/Dashboard.vue
@@ -7,7 +7,7 @@
         <div class="col-sm-6">
           <div class="card">
             <div class="card-body">
-              <input type="file" accept=".csv" @change="uploadFile">
+              <input type="file" accept=".xlsx" @change="uploadFile">
               <button @click="upload()">Upload</button>
               <p v-if="errorUpload" style="color: red">{{errorUpload}}</p>
             </div>
@@ -16,86 +16,6 @@
       </div>
     </div>
 
-    <div v-if="showSelectChart">
-      <div class="container mt-5" @click="selectChart('bar')">
-        <div class="row">
-          <div class="col-sm-6">
-            <div class="card">
-              <div class="card-body">
-                <img src="@/assets/vertical-chart.png" width="80%">
-                <p style="color: black">Vertical bar</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="container mt-5" @click="selectChart('line')">
-        <div class="row">
-          <div class="col-sm-6">
-            <div class="card">
-              <div class="card-body">
-                <img src="@/assets/line-chart.png" width="80%">
-                <p style="color: black">Line chart</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="container mt-5" @click="selectChart('scatter')">
-        <div class="row">
-          <div class="col-sm-6">
-            <div class="card">
-              <div class="card-body">
-                <img src="@/assets/scatter-chart.png" width="80%">
-                <p style="color: black">Scatter chart</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div v-if="showSelectAxes">
-      <div class="container mt-5">
-        <div class="row">
-          <div class="col-sm-6">
-            <div class="card">
-              <div class="card-body">
-                <p style="color: black">User id</p>
-                <select v-model="selectUserId">
-                  <option value="all">Tout</option>
-                  <option v-for="userId in usersId" :value="userId" :key="userId">
-                    {{ userId }}
-                  </option>
-                </select>
-                <p v-if="errors.user" style="color: red">{{errors.user}}</p>
-
-                <p style="color: black">Axe X</p>
-                <select v-model="axisX">
-                  <option v-for="title in titles" :value="title" :key="title">
-                    {{ title }}
-                  </option>
-                </select>
-                <p v-if="errors.user" style="color: red">{{errors.x}}</p>
-
-                <p style="color: black">Axe Y</p>
-                <select v-model="axisY">
-                  <option v-for="title in titles" :value="title" :key="title">
-                    {{ title }}
-                  </option>
-                </select>
-                <p v-if="errors.user" style="color: red">{{errors.y}}</p>
-
-                <button @click="generateGraph()">Générer le graphique</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <button @click="addGraph()">Ajouter le graphique au rapport</button>
     <div style="width: 1400px; height: 500px">
       <canvas id="graph"></canvas>
     </div>
