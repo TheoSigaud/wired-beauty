c75bbd026c4b3d1b941f98b6b031e3dfc87b0a48
Mame Ngom <mngom2@myges.fr>
2022-03-10 11:03:58 +0100

    [View/Admin]- Modifying PDF

--- client/src/components/Pdf/Pdf.js
+++ client/src/components/Pdf/Pdf.js
@@ -6,10 +6,16 @@ export default {
   name: 'Pdf',
   methods: {
     exportPdf: function (event) { 
-      doc.text("Rapport", 10, 10);
+      let product = document.getElementById("typeProduct").options[this.selectedIndex].text;
+      let chart = document.getElementById("typeChart").options[this.selectedIndex].text;
+      let option = document.getElementById("typeOption").options[this.selectedIndex].text;
 
-      doc.save("Rapport"+1+".pdf");
-
+      if(true){
+        doc.text("Comparer Moyenne de T0 à T1 avec pour critères 1 = Hydratant Moyenne sur (N = 1000) - Date début", 10, 10);
+      }
+      doc.text("Comparer Moyenne"+ +"de T0 à T1 avec pour critères 1 = Hydratant Moyenne sur (N = 1000) - Date début", 10, 10);
+      doc.save("Rapport"+doc+".pdf");
+      
     }
   }
 }
