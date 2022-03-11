f0524c99e3643bcec10882053afb6190e94442a7
Mame Ngom <mngom2@myges.fr>
2022-03-11 01:36:32 +0100

    Updating CSS

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,18 +1,17 @@
-import {getAuth, signOut} from "firebase/auth";
+import { getAuth, signOut } from "firebase/auth";
 const auth = getAuth();
 
 export default {
   name: 'Navbar',
 
-  data () {
+  data() {
     return {
       displayNavbar: false
     }
   },
   displayNavbar: false,
-
   methods: {
-    async logOut () {
+    async logOut() {
       signOut(auth).then(() => {
         this.$router.push({ name: 'Login' })
       })
