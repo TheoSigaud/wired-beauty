f81510537a787ab0ef771f0297ea0b451c4f8686
Mame Ngom <mngom2@myges.fr>
2022-03-12 20:40:13 +0100

    [Update/Report]- CSS

--- client/src/views/Report/Report.vue
+++ client/src/views/Report/Report.vue
@@ -1,24 +1,41 @@
 <template>
-  <div>
-    <router-view/>
-    <h1>Vos rapports</h1>
-
-    <table id="table" class="row-border hover">
-      <thead>
-      <tr>
-        <th>Nom</th>
-        <th>Actions</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-bind:key="report.id" v-for="report in reports">
-        <td style="color: white">{{ report.name }}</td>
-        <td>
-          <button @click="getPdf(report.name)">Voir</button>
-        </td>
-      </tr>
-      </tbody>
-    </table>
+  <div id="wrapper">
+    <router-view />
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div class="container-fluid">
+        <div class="">
+          <h1 class="h3 m-3 text-gray-800 font-weight-bold">Your Reports</h1>
+          <div class="row"></div>
+          <div
+            class="
+              row
+              d-sm-flex
+              align-items-center
+              justify-content-between
+              m-4
+              ml-5
+            "
+          >
+            <table id="table" class="hover">
+              <thead>
+                <tr>
+                  <th>Nom</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-bind:key="report.id" v-for="report in reports">
+                  <td style="color: white">{{ report.name }}</td>
+                  <td>
+                    <button @click="getPdf(report.name)">Voir</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
